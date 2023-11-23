6a91e30726be0d6c29ed78c04ca886f03f2016b7
Jimmy <117546160+Karcadian@users.noreply.github.com>
2023-11-22 21:54:00 -0600

    Update gh-pages branch

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = 'fdbec3a594e96a126a2b22810b451728';
+
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '286073bc7dmshb52c71751250054p101f59jsn157d47486eb2',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
